66b5bf584b5572a9f810fb2d1d88610008412edc
HuuThi12 <huuthi12072003@gmail.com>
2025-05-31 14:46:40 +0700

    update case study 1

--- Case_Study_2/src/main/java/com/techzen/academy_n0325c1/Bai9/repository/IDamageReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.techzen.academy_n0325c1.Bai9.repository;
-
-import com.techzen.academy_n0325c1.Bai9.model.DamageReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IDamageReportRepository extends JpaRepository<DamageReport, Integer> {
-}
